dde921f9d89d6f0c8ef5bc7e4317744058379289
da mao <msbox@live.cn>
2019-11-08 21:28:12 +0800

    处理内容去掉广告

--- app/Http/Service/TextHandleService.php
+++ app/Http/Service/TextHandleService.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Http\Service;
 
 
-class TextHandleService {
+class TextHandleService
+{
 
     /**
      * 解析内容数据
@@ -14,6 +16,8 @@ class TextHandleService {
         $txtData = str_replace("\n", "<br>", $txtData);
         $txtData = str_replace("style5();", "", $txtData);
         $txtData = str_replace("style6();", "", $txtData);
+        $txtData = preg_replace("/亲,点击进去,给个好评呗,分数越高更新越快\S*?无广告清新阅读！/",
+            "<br>", $txtData);
         $txtData .= <<< EOL
             <h5>
             本小程序所有小说均为转载作品，所有章节均由网友上传，本小程序只是为了宣传本书让更多读者欣赏，
